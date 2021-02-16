80313ead6ebc42fbe736653dd4a2366fc3298d8f
HelenLindberg <helenilindberg@yahoo.com>
2021-02-16 11:52:14 -0600

    prompts work

--- index.js
+++ index.js
@@ -6,30 +6,30 @@ inquirer.prompt([
     {
         type: 'input',
         name: 'username',
-        message: 'What is your username?'
+        message: 'What is your username?',
     },
     {
         type: 'input',
-        name: 'repo'
-        message: 'What is your gitHub repository?'
+        name: 'repo',
+        message: 'What is your gitHub repository?',
     },
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title for this project?'
+        message: 'What is the title for this project?',
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email?'
+        message: 'What is your email?',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Briefly describe your project.'
+        message: 'Briefly describe your project.',
     },
     {
-        type: 'checkbox';
+        type: 'checkbox',
         name: 'license',
         message: 'Choose your license:',
         choices: [
@@ -41,23 +41,27 @@ inquirer.prompt([
     {
         type: 'input',
         name: 'installation',
-        message: 'How do you install this application? What modules are needed?'
+        message: 'How do you install this application? What modules are needed?',
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Describe the usage of this application.'
+        message: 'Describe the usage of this application.',
     },
     {
         type: 'input',
         name: 'contributors',
-        message: 'List the contributors for this project, if any.'
+        message: 'List the contributors for this project, if any.',
     },
     {
         type: 'input',
-        name: 'test'
-        message: 'Comment on testing here:'
+        name: 'test',
+        message: 'Comment on testing here:',
     },
-
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please list any questions here:',
+    },
         
 ])
